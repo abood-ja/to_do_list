7e2c4d01f839f2b3abfa47f0417e7383621d783e
abood.jarrar <abd.a.jarrar@gmail.com>
2025-09-30 21:04:08 +0300

    select the add btn and the list

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 "use strict";
+const numberOfTasks = document.getElementById("statsText");
+const addBtn = document.getElementById("addBtn");
+const list = document.getElementById("todoList");
 class Task {
   #id;
-
-  constructor(title, expectedTime, urgetOrNot) {
-    this.title = title;
-    this.expectedTime = expectedTime;
-    this.urgetOrNot = urgetOrNot;
+  checked = false;
+  constructor(title) {
+    this._title = title;
+  }
+  get title() {
+    return this._title;
+  }
+  set title(title) {
+    this._title = title;
+  }
+  checkTask() {
+    this.checked = true;
+  }
+  isChecked() {
+    return this.checked;
   }
 }
