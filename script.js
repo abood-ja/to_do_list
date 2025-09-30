158b5b5954a4c6d8d2c790fdb8903d6be3529ab9
abood.jarrar <abd.a.jarrar@gmail.com>
2025-09-30 22:45:36 +0300

    finalyze implemnting the filter system

--- script.js
+++ script.js
@@ -2,11 +2,54 @@
 const numberOfTasks = document.getElementById("statsText");
 const addBtn = document.getElementById("addBtn");
 const list = document.getElementById("todoList");
+const stateBtns = document.querySelector(".filters");
+const textField = document.getElementById("todoInput");
+const allStateBtns = [...document.querySelectorAll(".filter-btn")];
+const allTasks = [];
+function addNewTask(e) {
+  const task = new Task(textField.value);
+  allTasks.push(task);
+}
+function renderTasks(filteredTasksHTML) {
+  list.innerHTML = "";
+  filteredTasksHTML.forEach(
+    (task) => (list.innerHTML += task.createTaskHTML())
+  );
+}
+function changeActiveBtn(e) {
+  const target = e.target;
+  if (!target.classList.contains("filter-btn")) return;
+  allStateBtns.forEach((ele) => {
+    ele.classList.remove("active");
+  });
+  target.classList.add("active");
+}
+function showList(e) {
+  const target = e.target;
+  if (!target.classList.contains("filter-btn")) return;
+  const type = target.dataset.filter;
+  let filteredTasksHTML = [];
+  if (type == "all") {
+    filteredTasksHTML = allTasks.map((task) => task.createTaskHTML());
+    renderTasks(filteredTasksHTML);
+  } else if (type == "active") {
+    const filteredTasks = allTasks.filter((task) => !task.isChecked());
+    filteredTasksHTML = filteredTasks.map((task) => task.createTaskHTML());
+    renderTasks(filteredTasksHTML);
+  } else {
+    const filteredTasks = allTasks.filter((task) => task.isChecked());
+    filteredTasksHTML = filteredTasks.map((task) => task.createTaskHTML());
+    renderTasks(filteredTasksHTML);
+  }
+}
 class Task {
   #id;
+  static #counter = 0;
+
   checked = false;
   constructor(title) {
     this._title = title;
+    this.#id = ++Task.#counter;
   }
   get title() {
     return this._title;
@@ -14,10 +57,25 @@ class Task {
   set title(title) {
     this._title = title;
   }
+  get id() {
+    return this.#id;
+  }
   checkTask() {
     this.checked = true;
   }
   isChecked() {
     return this.checked;
   }
+  createTaskHTML() {
+    return `
+        <li class="todo-item">
+          <input type="checkbox" class="checkbox" />
+          <span class="todo-text" data-id="${this.id}">${this.title}</span>
+          <button class="delete-btn">Delete</button>
+        </li>
+    `;
+  }
 }
+stateBtns.addEventListener("click", changeActiveBtn);
+stateBtns.addEventListener("click", showList);
+addBtn.addEventListener("click", addNewTask);
